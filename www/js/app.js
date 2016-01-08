786f97c57757790831a9f7395ee24922c7fdc7a9
Marcos Pirani <marcos.pir94@gmail.com>
2016-01-08 15:56:30 -0300

    navegacion terminada

--- www/js/app.js
+++ www/js/app.js
@@ -13,17 +13,17 @@ angular.module('starter', ['ionic'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  var rout = $stateProvider;
+  $stateProvider
 
   //estado abtracto que corresponde a la barra de navegacion
-    rout.state('tab', {
+    .state('tab', {
       url: '/tab',
       abstract: true,
       templateUrl: "templates/tab.html"
     })
 
   //cada estado que sigue va a heredad de la plantilla tab. Encontes sera la ruta "/tab/home"
-    rout.state('tab.home', {
+    .state('tab.home', {
       url: '/home',
       views:{
         //name que va en el ion-nav-view en tabs.html
@@ -32,8 +32,38 @@ angular.module('starter', ['ionic'])
         }
       }
     })
-
-
+    .state('tab.auto', {
+      url: '/auto',
+      views:{
+        'tab-auto':{
+          templateUrl: "templates/auto.html"
+        }
+      }
+    })
+    .state('tab.comunidad', {
+      url: '/comunidad',
+      views:{
+        'tab-comunidad':{
+          templateUrl: "templates/comunidad.html"
+        }
+      }
+    })
+    .state('tab.datos', {
+      url: '/datos',
+      views:{
+        'tab-datos':{
+          templateUrl: "templates/datos.html"
+        }
+      }
+    })
+    .state('tab.info', {
+      url: '/info',
+      views:{
+        'info':{
+          templateUrl: "templates/info.html"
+        }
+      }
+    })
 
   $urlRouterProvider.otherwise('/tab/home');
 })
